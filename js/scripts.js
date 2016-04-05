cc2e28ee5c514a5c7b85c7d04b7a763ce9124d83
Daniel Wulff and Inthrayuth Mahaphol <inthrayuth@>
2016-04-05 14:39:46 -0700

    business logic and UI logic finish

--- js/scripts.js
+++ js/scripts.js
@@ -5,28 +5,45 @@ var BankAccount = function(name, initDeposit) {
 }
 
 BankAccount.prototype.deposit = function(depositAmount) {
-  balance += depositAmount;
+  this.balance += depositAmount;
 }
 
 BankAccount.prototype.withdraw = function(withdrawAmount) {
-  balance -= withdrawAmount;
+  this.balance -= withdrawAmount;
 }
 
 // user interface logic
 $(document).ready(function() {
+  var printBalance = function() {
+    $("#balance").text(customerAccount.balance);
+  }
+
+  var customerAccount = undefined;
+
   $("#sign-up").submit(function(event) {
     event.preventDefault();
-    
+    var name = $("#name").val();
+    var initDeposit = parseInt($("#initDeposit").val());
+    customerAccount = new BankAccount(name, initDeposit);
 
+    printBalance();
+
+    $("#name").val("");
+    $("#initDeposit").val("");
   });
 
   $("#transaction").submit(function(event) {
     event.preventDefault();
+    var depositAmount = parseInt($("#deposit").val());
+    customerAccount.deposit(depositAmount);
+    var withdrawAmount = parseInt($("#withdraw").val());
+    customerAccount.withdraw(withdrawAmount);
 
+    printBalance();
 
-
+    $("#deposit").val("");
+    $("#withdraw").val("");
   });
-
 
 
 });
